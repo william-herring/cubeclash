4da615867b6cab4da419a16f494c2b52f5597a98
williamherring <william.herring.au@gmail.com>
2026-01-12 14:09:43 +1100

    revised login design

--- users/views.py
+++ users/views.py
@@ -1,20 +1,5 @@
-from django.contrib.auth import login
-from django.contrib.auth.forms import AuthenticationForm
-from django.shortcuts import render, redirect
-from django.views import View
+from django.views.generic import TemplateView
 
 
-class LoginView(View):
-    def post(self, request, *args, **kwargs):
-        form = AuthenticationForm(data=request.POST)
-
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('/')
-        else:
-            return render(request, 'login.html', {'form': form, 'invalid_details': True})
-
-    def get(self, request):
-        form = AuthenticationForm()
-        return render(request, 'login.html', {'form': form})
+class LoginView(TemplateView):
+    template_name = 'login.html'
